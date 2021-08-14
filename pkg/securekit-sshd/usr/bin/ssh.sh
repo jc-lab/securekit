8c993738e2a108bdbf11b849543a46c5bf02bc8f
jichan <joseph@jc-lab.net>
2021-08-14 23:44:43 +0900

    fix: ONLY_SFTP option

--- pkg/securekit-sshd/usr/bin/ssh.sh
+++ pkg/securekit-sshd/usr/bin/ssh.sh
@@ -4,7 +4,7 @@ KEYS=$(find /etc/ssh -name 'ssh_host_*_key')
 [ -z "$KEYS" ] && ssh-keygen -A >/dev/null
 
 cat /etc/ssh/sshd_config.in > /tmp/sshd_config
-[ "x${ONLY_SFTP:-yes}" = "xno" ] && cat >> /tmp/sshd_config << EOF
+[ "x${ONLY_SFTP:-yes}" = "xno" ] || cat >> /tmp/sshd_config << EOF
 
 AllowTcpForwarding no
 ForceCommand internal-sftp
