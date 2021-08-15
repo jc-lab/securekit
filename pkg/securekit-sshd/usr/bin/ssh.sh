b5c17b6f522f3a257155fca6170b0fdacaa85f48
jichan <joseph@jc-lab.net>
2021-08-15 21:15:02 +0900

    refactor: remove init-ssh

--- pkg/securekit-sshd/usr/bin/ssh.sh
+++ pkg/securekit-sshd/usr/bin/ssh.sh
@@ -3,7 +3,7 @@
 KEYS=$(find /etc/ssh -name 'ssh_host_*_key')
 [ -z "$KEYS" ] && ssh-keygen -A >/dev/null
 
-cat /etc/ssh/sshd_config.in > /tmp/sshd_config
+cat /etc/sshd_config.in > /tmp/sshd_config
 [ "x${ONLY_SFTP:-yes}" = "xno" ] || cat >> /tmp/sshd_config << EOF
 
 AllowTcpForwarding no
